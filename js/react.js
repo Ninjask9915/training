64ff257603fbd50f1dd3c64262cfcc1b1c06bfb7
Ninjask9915 <waterfall9915a@gmail.com>
2023-04-27 07:02:07 +0900

    20230427

--- js/react.js
+++ js/react.js
@@ -14,5 +14,13 @@ Vue.createApp({
     // 終了時にタイマーを破棄
     beforeUnmount() {
         clearInterval(this.timer);
+    },
+    renderTracked(e) {
+        console.log('renderTracked');
+        console.log(e)
+    },
+    renderTriggered(e) {
+        console.log('renderTriggered');
+        console.log(e);
     }
 }).mount('#app');
